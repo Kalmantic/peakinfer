f3419f72a2f0c5c47f9a038c285527ddd642bdcb
badhra-ajaz <mail>
2025-11-19 14:21:11 +0530

    removed dist files

--- dist/types/template.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-/**
- * Template Types - Based on TokenSqueeze insights and 27 existing templates
- * Templates encode optimization knowledge that Claude Code SDK agents can execute
- */
-Object.defineProperty(exports, "__esModule", { value: true });
